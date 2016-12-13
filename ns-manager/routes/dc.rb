fb95e55526b22d294bc8879308f7f71f74e83f98
Alfonso Egio <alfonso.egio@gmail.com>
2016-12-13 09:03:41 +0100

    network retrieving

--- ns-manager/routes/dc.rb
+++ ns-manager/routes/dc.rb
@@ -32,6 +32,43 @@ class DcController < TnovaManager
         end
     end
 
+    # @method get dc_networks
+    # @overload get "/networks/:id"
+    # Get the networks avaliable for the vim (ANELLA)
+    get '/networks/:id' do |id|
+        begin
+            begin
+                dc = Dc.find(id.to_i)
+            rescue Mongoid::Errors::DocumentNotFound => e
+                logger.error 'DC not found'
+                return 404
+            end
+            popUrls = getPopUrls(dc['extra_info'])
+            puts popUrls
+            neutron_url = popUrls[:neutron]
+            admin_credentials, errors = authenticate_anella(popUrls[:keystone], dc["tenant_name"], dc['user'], dc['password'])
+            tenant_id = admin_credentials[:tenant_id]
+            auth_token = admin_credentials[:token]
+            begin
+                puts neutron_url
+                puts auth_token
+                response = RestClient.get neutron_url +"/networks", 'X-Auth-Token' => auth_token, :accept => :json
+            rescue Errno::ECONNREFUSED
+                # halt 500, 'VIM unreachable'
+                logger.error "VIM unreachable"
+            rescue RestClient::ResourceNotFound
+                logger.error 'Already removed from the VIM.'
+                return 404
+            rescue => e
+                logger.error e
+                #logger.error e.response
+                return
+                # halt e.response.code, e.response.body
+            end
+            response
+        end
+    end
+
     # @method get dc_flavours
     # @overload get "/flavours/:id"
     # Get the flavours avaliable for the vim (ANELLA)
@@ -46,7 +83,6 @@ class DcController < TnovaManager
             popUrls = getPopUrls(dc['extra_info'])
             compute_url = popUrls[:compute]
             admin_credentials, errors = authenticate_anella(popUrls[:keystone], dc["tenant_name"], dc['user'], dc['password'])
-            puts admin_credentials
             tenant_id = admin_credentials[:tenant_id]
             auth_token = admin_credentials[:token]
             begin
