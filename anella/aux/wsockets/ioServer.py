84d49b03ab996f773b78c545b6a127ef57eca6a3
Alfonso Egio <alfonso.egio@gmail.com>
2016-12-16 10:13:48 +0100

    ws on flask working fine

--- anella/aux/wsockets/ioServer.py
+++ anella/aux/wsockets/ioServer.py
@@ -3,6 +3,7 @@
 
 import flask_restful
 from flask import Flask, Blueprint, request, session, render_template
+from flask import send_from_directory
 from flask_restful import Api, abort
 from flask_cors import CORS, cross_origin
 from flask_socketio import SocketIO
@@ -26,30 +27,18 @@ ORCHESTRATOR_URL = 'http://localhost:8082/orchestrator/api/v0.1/service/instance
 def index():
     return render_template('own.html', async_mode=SOCKETIO.async_mode)
 
-# class Launch(flask_restful.Resource):
-#     def post(self):
-#         data = request.get_json()
-#         response = requests.post(ORCHESTRATOR_URL,
-#                                  headers={'Content-Type': 'application/json'},
-#                                  json=data)
-#         resp = json.loads(response.text)
-#         print resp['service_instance_id']
-#         return 200
-
+@APP.route('/static/<path:path>')
+def send_js(path):
+    return send_from_directory('static', path)
 
 @SOCKETIO.on('my_event')
 def test_message(message):
     session['receive_count'] = session.get('receive_count', 0) + 1
-    print "LKJASDLKJASD"
-    # emit('my_response',
-    #      {'data': message['data'], 'count': session['receive_count']})
-
-# API_V2.add_resource(Launch,'/launch')
+    print message['data']
+    emit('my_response',
+         {'data': message['data'], 'count': session['receive_count']})
 
 if __name__ == "__main__":
     print "IO server"
-    # APP.register_blueprint(
-    #     API_V2_BP,
-    #     url_prefix=URL_PREFIX
-    # )
     SOCKETIO.run(APP, debug=True)
+
