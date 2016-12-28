c878f8aa59118a7540c50b44a52573a07a9e13e5
Alfonso Egio <alfonso.egio@gmail.com>
2016-12-28 13:11:02 +0100

    cleandb

--- anella/tenor_client/cleandb.py
+++ anella/tenor_client/cleandb.py
@@ -11,3 +11,9 @@ ns_catalogue = client['ns_catalogue']
 ns_catalogue.ns.remove()
 vnf_catalogue = client['vnf_catalogue']
 vnf_catalogue.vnfs.remove()
+
+pi40orch = client['pi40orch']
+pi40orch.regular_message.remove()
+pi40orch.critical_error.remove()
+pi40orch.api_log.remove()
+pi40orch.instance_configuration.remove()
